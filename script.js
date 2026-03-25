fbfe6317b0e6043e4217f04d53cdd129d55a68ba
Aruna .Ch <chadalavadaaruna197@gmail.com>
2026-03-25 17:05:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("Thanks for your interest! 😊");
+}
+
+function submitForm() {
+    alert("Message Sent Successfully!");
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
